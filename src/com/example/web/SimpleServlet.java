795987c58fcfd322b597bf0273057030030d332b
Marina Sljivic <BILD-IT@BILD-IT-PC2>
2015-10-25 08:50:00 +0100

    some corrections

--- src/com/example/web/SimpleServlet.java
+++ src/com/example/web/SimpleServlet.java
@@ -34,7 +34,7 @@ public class SimpleServlet extends HttpServlet {
 		out.println("<html><head><title>Your form</title>" + "<link rel=\"stylesheet\" "
 				+ "href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">"
 				+ "<link rel=\"stylesheet\" href=\"nicestyle.css\">" + "</head><body>");
-		out.println("<div class=\"row\"><div class=\"col-sm-6\"><img src=\"smajlici.jpg\"/></div>"
+		out.println("<div class=\"row\"><div class=\"col-sm-6\"><img src=\"smajlici.jpg\" class=\"img-thumbnail\" /></div>"
 				+ "<div class=\"col-sm-6\"><div class=\"naslovi\"><h1>SMIJEH</h1>"
 				+ "<h2>Najmocnija vjestina na svijetu</h2></div></div></div>");
 		
@@ -48,18 +48,18 @@ public class SimpleServlet extends HttpServlet {
 		if(name.trim()=="" || surname.trim()=="" || dateOfBirth.trim()=="" || phoneNumber.trim()==""
 				|| email.trim()=="" || address.trim()==""){
 			
-			out.println("<div class=\"paragrafi\"><p class=\"uvod\">Niste dobro ispunili prijavu, vratite se nazad.</p><br/></div>");
+			out.println("<div class=\"paragrafi\"><p>Niste dobro ispunili prijavu, vratite se nazad.</p><br/></div>");
 			
 		}else{
-			out.println("<div class=\"paragrafi\"><p class=\"uvod\">Uspjesno ste poslali Vase podatke.</p><br/></div>");
+			out.println("<div class=\"paragrafi\"><p>Uspjesno ste poslali Vase podatke.</p><br/></div>");
 			out.println("<div class=\"container\">" + "<table class=\"table\">" + "<thead>" + "<tr>"
 					+ "<th>Ime</th> <th>Prezime</th> <th>Datum rodjenja</th> <th>Broj telefona</th> <th>Email</th> <th>Adresa</th>"
 					+ "</tr>" + "</thead>" + "<tbody>" + "<tr>" + "<td>" + name + "</td>" + "<td>" + surname + "</td>"
 					+ "<td>" + dateOfBirth + "</td>" + "<td>" + phoneNumber + "</td>" + "<td>" + email + "</td>" + "<td>"
 					+ address + "</td>" + "</tr>" + "</tbody></table></div>");
-			out.println("<div class=\"paragrafi\"><p class=\"glavni dio\"p><img src=\"takesmile.png\" class=\"img-rounded\" alt=\"take a smile\""
+			out.println("<img src=\"takesmile.png\" class=\"img-thumbnail\" alt=\"take a smile\""
 					+ "align=\"left\" width=\"300\" height=\"100\" />"
-					+ "</p></div><br/><br/>");
+					+ "<br/><br/>");
 			out.println("</body></html>");
 		}
 		
